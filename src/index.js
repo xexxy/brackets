5e1c4c064fbeb82f8d1b780cd93d3dc39927b307
xexxy <38164075+xexxy@users.noreply.github.com>
2020-10-25 20:27:35 +0300

    feat: add part of functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [],
+        brackets = '(){}[][|';
+
+    for (let bracket of str) {
+        let bracketIndex = brackets.indexOf(bracket),
+        haveBracket = false;
+
+        if (bracketIndex == -1) {
+            return false;
+        } else if (bracketIndex == 7 && bracketIndex != stack[stack.length - 1]) {
+            stack.push(bracketIndex);
+            haveBracket = true;
+        } else if (bracketIndex % 2 == 0) {
+            stack.push(bracketIndex + 1);
+            haveBracket = true;
+        } else {
+            haveBracket = true;
+            if (stack.pop() != bracketIndex) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length == 0;
+
 }
